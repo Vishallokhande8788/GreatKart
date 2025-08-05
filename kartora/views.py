f6c59d2f78f405d5fe2d921bcfe893058f0c160f
vishal lokhande <lvishal108@gmail.com>
2025-08-05 05:26:25 +0000

    cretat virtual env , start project kartora , creat home app , update settings , creat view , set url ,

--- kartora/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-
-from django.http import HttpResponse
-
-def hello_view(request):
-    return HttpResponse("Hello, Vishal! Your Django setup works perfectly.")
