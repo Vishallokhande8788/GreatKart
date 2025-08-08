e6909c4109bf36cb4a2f57883de147116762c38f
vishal lokhande <lvishal108@gmail.com>
2025-08-08 16:22:31 +0000

    Implement custom Account model with MyAccountManager, admin configuration, and fix slug prepopulation in CategoryAdmin

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home',
     'category',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -71,6 +72,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'greatkart.wsgi.application'
+AUTH_USER_MODEL = 'accounts.Account'
+
 
 
 # Database
@@ -124,6 +127,11 @@ STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
 'greatkart/static',
 ]
+
+# media files configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
--- greatkart/urls.py
+++ greatkart/urls.py
@@ -17,8 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from home import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+MEDIA_ROOT = settings.MEDIA_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
-]
+]+ static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
