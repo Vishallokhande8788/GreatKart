8da2b9a1a8657a9896a1ddf127730e622637c62b
vishal lokhande <lvishal108@gmail.com>
2025-09-19 06:47:23 +0000

    get urls for product

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -21,10 +21,11 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
 
+
 MEDIA_ROOT = settings.MEDIA_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
+    path('', views.home , name='home'),
     path('store/',include('store.urls')),
 ]+ static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
